ec3554339af260f46e2d640703dd90e3c31024c8
stefansor <ssorobay00@stuy.edu>
2019-04-02 14:47:45 -0400

    added a max and find max digits so nothing really done

--- Radix.java
+++ Radix.java
@@ -1,27 +1,41 @@
-import.java.util.*;
+import java.util.*;
 
 public class Radix{
 
 
-  //helper methods such as getDigit() and others
+  public static int max(int[] nums){
+    int largest = nums[0];
+    for(int i = 0 ; i < nums.length; i++){
+      if(nums[i] > largest){
+        largest = nums[i];
+      }
+    }
+    return largest;
+  }
 
-  public static int getDigit(int num, int place){
-    int divider = Math.pow(10, place);
-    return (num / divider) % 10;
+  public static int numOfDigits(int num){
+    int digits = 0;
+    while(num > 10){
+      num = num / 10;
+      digits++;
+    }
+    digits++;
+    return digits;
   }
 
 
-
-
-
-
   public static void radixsort(int[]data){
+    MyLinkedList[] buckets = new MyLinkedList[20];
 
   }
 
 
   public static void main(String[] args){
-      System.out.println(getDigit(34, 2));
+    int[] stuff = new int[]{
+      3,2,4,5,4,3,2,23432,334,2344
+    };
+    System.out.println(max(stuff));
+    System.out.println(numOfDigits(32432));
   }
 
 
