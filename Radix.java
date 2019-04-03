86db6453b0969ae42c88bd5e0c715eee5ff5773c
stefansor <ssorobay00@stuy.edu>
2019-04-03 17:50:33 -0400

    added getDigits and will write a method that sorts the array of buckets by digits then another method to link the arranged buckets and the last function to copy the linked list into the data

--- Radix.java
+++ Radix.java
@@ -24,6 +24,11 @@ public class Radix{
   }
 
 
+  public static int getDigit(int num, int place){
+    return (num / (int)Math.pow(10, place)) % 10;
+  }
+
+
   public static void radixsort(int[]data){
     MyLinkedList[] buckets = new MyLinkedList[20];
     int highest = max(data);
@@ -34,12 +39,30 @@ public class Radix{
   }
 
 
+
+
+
+  ///Plan -- inspired by Mohammad Jawwad - will work later tonight
+  // 1. write a method that sorts the digits in each bucket for each set
+  //  of digits
+  // 2. write method that links the filled buckets
+  // 3. write method that copies linked list into data
+
   public static void main(String[] args){
     int[] stuff = new int[]{
       3,2,4,5,4,3,2,23432,334,2344
     };
     System.out.println(max(stuff));
     System.out.println(numOfDigits(32432));
+    System.out.println(getDigit(32432, 0));
+    System.out.println(getDigit(32432, 1));
+    System.out.println(getDigit(32432, 2));
+    System.out.println(getDigit(32432, 3));
+    System.out.println(getDigit(32432, 4));
+    System.out.println(getDigit(32432, 5));
+
+
+
   }
 
 
