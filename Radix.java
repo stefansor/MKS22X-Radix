c2fa12f192ffbe1cf3b80c8035843b589997dc35
stefansor <ssorobay00@stuy.edu>
2019-04-03 21:41:05 -0400

    finished method that sorts the digits into buckets by digits

--- Radix.java
+++ Radix.java
@@ -31,10 +31,19 @@ public class Radix{
 
   public static void radixsort(int[]data){
     MyLinkedList[] buckets = new MyLinkedList[20];
+    for(int i = 0; i < 20; i++){
+      buckets[i] = new MyLinkedList();
+    }
+
     int highest = max(data);
     int times = numOfDigits(highest);
+
     for(int i = 0; i < times; i++){
-      ////code that distributes the numbers according to digits
+      //function that sorts by the chosen digits
+      //function that links the buckets
+      //funciton that copies to data
+
+
     }
   }
 
@@ -48,11 +57,28 @@ public class Radix{
   // 2. write method that links the filled buckets
   // 3. write method that copies linked list into data
 
+  private static MyLinkedList[] sort(int[] data, int place, MyLinkedList[] buckets){
+    for(int i = 0; i < data.length; i++){
+      if(data[i] < 0){
+        buckets[9 - Math.abs(getDigit(data[i], place))].add(data[i]);
+      }
+      else{
+        buckets[10 + Math.abs(getDigit(data[i], place))].add(data[i]);
+      }
+    }
+    return buckets;
+  }
+
+
+
   public static void main(String[] args){
-    int[] stuff = new int[]{
+    int[] yea = new int[]{
       3,2,4,5,4,3,2,23432,334,2344
     };
-    System.out.println(max(stuff));
+    MyLinkedList[] stuff = new MyLinkedList[20];
+    for(int i = 0; i < 20; i++){
+      stuff[i] = new MyLinkedList();
+    }
     System.out.println(numOfDigits(32432));
     System.out.println(getDigit(32432, 0));
     System.out.println(getDigit(32432, 1));
@@ -60,7 +86,11 @@ public class Radix{
     System.out.println(getDigit(32432, 3));
     System.out.println(getDigit(32432, 4));
     System.out.println(getDigit(32432, 5));
-
+    System.out.println("//////////////////////////");
+    sort(yea, 2, stuff);
+    for(int i = 0; i < 20; i++){
+      System.out.println(stuff[i]);
+    }
 
 
   }
